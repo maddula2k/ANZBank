ce3c9069db9f598c177f915c3baa4e507bf108ae
sreevini <sreevini@LAPTOP-OMQFTHCM>
2021-04-22 15:16:39 -0400

    Testing2

--- src/anz/Test.java
+++ src/anz/Test.java
@@ -3,8 +3,8 @@ package anz;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("github testing");
-
+		System.out.println("github testing1");
+        System.out.println("github testing2");
 	}
 
 }
